bed06caf91b3ae912cbedd35012f34e18c5c2a54
Nicholas Jensen and Michael Smith <student@epicodus.com>
2016-02-18 11:28:45 -0800

    returns day of the week for particular date using a calculation.

--- Objects/DayOfWeek.cs
+++ Objects/DayOfWeek.cs
@@ -4,8 +4,37 @@ namespace DayOfWeekNS.Objects
 {
   public class DayOfWeek
   {
+    private static Dictionary<int, string> _days = new Dictionary<int,string> {
+      {6, "Sunday"},
+      {0, "Monday"},
+      {1, "Tuesday"},
+      {2, "Wedensday"},
+      {3, "Thursday"},
+      {4, "Friday"},
+      {5, "Saturday"}
+    };
     public DayOfWeek()
     {
+    }
+
+    public static string Do(int month, int day, int year)
+    {
+      int index = month + (day % 7) + YearCode(year);
+      return _days[index % 7];
+    }
+
+    public static int YearCode(int year)
+    {
+      int yearCode = 0;
+      for(int i = 2001;i<=year;i++)
+      {
+        if(year % 4 == 0 && !(year % 100 == 0) || year % 400 == 0)
+        {
+          yearCode += 2;
+        } else yearCode += 1;
+      }
+      Console.WriteLine("Year: " +year+ " YearCode: " + yearCode % 7);
+      return yearCode % 7;
     }
   } // end class
 } // end namespace
--- Tests/DayOfWeekTest.cs
+++ Tests/DayOfWeekTest.cs
@@ -4,6 +4,32 @@ namespace DayOfWeekNS
 {
   public class DayOfWeekTest
   {
+    [Fact]
+    public void test1_enter_todaysDate_and_return_thursday()
+    {
+      Assert.Equal("Thursday", DayOfWeek.Do(2,18,2016));
+    }
+    [Fact]
+    public void test2_enter_todaysDate_and_return_thursday()
+    {
+      Assert.Equal("Thursday", DayOfWeek.Do(1,1,2015));
+    }
+    [Fact]
+    public void test3_enter_todaysDate_and_return_thursday()
+    {
+      Assert.Equal("Thursday", DayOfWeek.Do(4,18,2030));
+    }
+    [Fact]
+    public void test4_enter_todaysDate_and_return_thursday()
+    {
+      Assert.Equal("Saturday", DayOfWeek.Do(5,20,2069));
+    }
+    [Fact]
+    public void test5_enter_todaysDate_and_return_thursday()
+    {
+      Assert.Equal("Thursday", DayOfWeek.Do(12,14,2000));
+    }
+
     /* EXAMPLE
     // Have a queen object that knows what coordinants its at
     [Fact]
